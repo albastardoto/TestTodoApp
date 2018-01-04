e736adbccf4ec47bb0c319d9a3e7fc75f2db3e11
Albert Koppelmaa <albert.koppelmaa@estcube.eu>
2018-01-04 15:42:30 +0100

    bug fixes

--- Server/db/mongoose.js
+++ Server/db/mongoose.js
@@ -1,5 +1,6 @@
 var mongoose= require("mongoose");
 mongoose.Promise= global.Promise;
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp", {useMongoClient:true});
-
+//mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp", {useMongoClient:true});
+mongoose.connect("mongodb://heroku_jkhqmgqz:najtf94a6tk2tuqb5dvomft43u@ds237947.mlab.com:37947/heroku_jkhqmgqz",
+{useMongoClient:true});
 module.exports={ mongoose }
