a34ffb4e9c2951b5caeaa0ef9854e9ede35a92f6
Albert Koppelmaa <albert.koppelmaa@estcube.eu>
2018-01-05 11:41:35 +0100

    added delete todo functionality

--- Server/server.js
+++ Server/server.js
@@ -47,7 +47,24 @@ app.get("/todos/:id",(req,res)=>{
   })
 });
 
+app.delete("/todos/:id",(req,res)=>{
+  let id = req.params.id;
+  if(!ObjectId.isValid(id)){
+    return res.status(404).send();
+  }
+  Todo.findByIdAndRemove(id).then((todo)=>{
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e)=>{
+    res.status(400).send(e);
+  });
+});
+
 app.listen(port,()=>{
   console.log(`Started on port ${port}`);
 })
+
+
 module.exports={app};
