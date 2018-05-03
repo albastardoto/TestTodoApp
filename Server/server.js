2993b2453ab932e4698cf7cdb60e1c154530d5ea
Albert Koppelmaa <albert.koppelmaa@estcube.eu>
2018-05-03 15:41:58 +0200

    fixed cors

--- Server/server.js
+++ Server/server.js
@@ -20,6 +20,7 @@ app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Auth');
+  res.setHeader('Access-Control-Expose-Headers', 'X-Auth');
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });  
